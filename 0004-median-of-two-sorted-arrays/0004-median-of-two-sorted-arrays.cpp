1f79bdb7233e839988a583c60065ca14472a4035
Shivam <officialxshivam45@gmail.com>
2025-09-26 22:32:32 +0530

    Time: 0 ms (100%), Space: 94.8 MB (99.61%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,56 +1,30 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int cnt = 0;
-        int i = 0, j = 0;
         int n1 = nums1.size(), n2 = nums2.size();
+        // ensure nums1 is smaller
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
         int n = n1 + n2;
-        double ele1 = -1, ele2 = -1;
-        while (i < n1 && j < n2) {
-            if (nums1[i] <= nums2[j]) {
-                cnt++;
-                if (cnt == n / 2)
-                    ele1 = nums1[i];
-                if (cnt - 1 == n / 2) {
-                    ele2 = nums1[i];
-                    break;
-                }
-                i++;
-            } else {
-                cnt++;
-                if (cnt == n / 2)
-                    ele1 = nums2[j];
-                if (cnt - 1 == n / 2) {
-                    ele2 = nums2[j];
-                    break;
-                }
-                j++;
-            }
-        }
 
-        while (i < n1) {
-            cnt++;
-            if (cnt == n / 2)
-                ele1 = nums1[i];
-            if (cnt - 1 == n / 2) {
-                ele2 = nums1[i];
-                break;
-            }
-            i++;
-        }
-        while (j < n2) {
-            cnt++;
-            if (cnt == n / 2)
-                ele1 = nums2[j];
-            if (cnt - 1 == n / 2) {
-                ele2 = nums2[j];
-                break;
-            }
-            j++;
-        }
+        int low = 0, high = n1;
+        while (low <= high) {
+            int x = (low + high) / 2;
+            int left = n / 2 - x;
 
-        if (n % 2)
-            return ele2;
-        return (ele1 + ele2) / 2;
+            int l1 = (x == 0 ? INT_MIN : nums1[x - 1]);
+            int r1 = (x == n1 ? INT_MAX : nums1[x]);
+            int l2 = (left == 0 ? INT_MIN : nums2[left - 1]);
+            int r2 = (left == n2 ? INT_MAX : nums2[left]);
+
+            if (max(l1, l2) <= min(r1, r2))
+                return (n % 2) ? min(r1, r2)
+                               : (max(l1, l2) + min(r1, r2)) / 2.0;
+            else if (l1 > r2)
+                high = x - 1;
+            else
+                low = x + 1;
+        }
+        return -1;
     }
 };
