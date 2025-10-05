7e11f71b341038d90d7b03f6fa680f700db82d53
Shivam <officialxshivam45@gmail.com>
2025-10-05 10:44:42 +0530

    Time: 0 ms (100%), Space: 9.1 MB (23.39%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -3,21 +3,16 @@ public:
     string removeOuterParentheses(string s) {
         int n = s.length();
         int cnt = 0;
-        vector<string> res;
-        string s1;
-        for(int i = 0; i < n; i++){
-            if(s[i] == '(') cnt++;
-            else cnt--;
-            s1 += s[i];
-            if(cnt == 0){
-                res.push_back(s1);
-                s1 = "";
-            }
-        }
         string output;
-        for(int i = 0; i < res.size(); i++){
-            int m = res[i].length();
-            output += res[i].substr(1, m - 2);
+        for(int i = 0; i < n; i++){
+            if(s[i] == '('){
+                cnt++;
+                if(cnt != 1) output.push_back(s[i]);
+            }
+            else{
+                cnt--;
+                if(cnt != 0) output.push_back(s[i]);
+            }
         }
         return output;
     }
