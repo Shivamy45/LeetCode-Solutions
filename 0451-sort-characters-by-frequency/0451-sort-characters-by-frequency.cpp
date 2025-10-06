d28b56212be5db6ad63f0e9d0c65ed34f985e758
Shivam <officialxshivam45@gmail.com>
2025-10-06 19:48:08 +0530

    Time: 4 ms (37.57%), Space: 11.2 MB (50.67%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        vector<pair<char, int>> vec(256);
+        vector<pair<char, int>> freq(256);
 
         for (auto& c : s) {
-            int fr = vec[c].second;
-            vec[c] = {c, fr + 1};
+            int fr = freq[c].second;
+            freq[c] = {c, fr + 1};
         }
 
-        sort(vec.begin(), vec.end(),
+        sort(freq.begin(), freq.end(),
              [](auto& a, auto& b) { return a.second > b.second; });
 
         string res;
-        for (auto& it : vec) {
-            res += string(it.second, it.first);
+
+        for (int i = 0; i < 256; i++) {
+            if (freq[i].second > 0) {
+                char t = freq[i].first;
+                int f = freq[i].second;
+                string temp = string(f, t);
+                res += temp;
+            }
         }
         return res;
     }
