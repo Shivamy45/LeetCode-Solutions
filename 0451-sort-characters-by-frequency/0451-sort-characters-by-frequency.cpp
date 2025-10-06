7768893351da0e7c7842fa8e2b3e1695750b66a6
Shivam <officialxshivam45@gmail.com>
2025-10-06 19:49:19 +0530

    Time: 0 ms (100%), Space: 11.3 MB (39.6%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    typedef pair<char,int>p;
     string frequencySort(string s) {
-        string ans = "";
-        vector<p> freq(256);
-        
-        for(auto &c : s){
+        vector<pair<char, int>> freq(256);
+
+        for (auto& c : s) {
             int fr = freq[c].second;
-            freq[c] = {c,fr+1};
+            freq[c] = {c, fr + 1};
         }
 
-        auto lamda = [&](p &p1,p &p2){
-            return p1.second>p2.second;
-        };
+        sort(freq.begin(), freq.end(),
+             [](auto& a, auto& b) { return a.second > b.second; });
 
-        sort(freq.begin(),freq.end(),lamda);
+        string res = "";
 
-        for(int i = 0;i<256;i++){
-            if(freq[i].second > 0){
-            char t = freq[i].first;
-            int f = freq[i].second;
-            string temp = string(f,t);
-            ans += temp;
+        for (int i = 0; i < 256; i++) {
+            if (freq[i].second > 0) {
+                char t = freq[i].first;
+                int f = freq[i].second;
+                string temp = string(f, t);
+                res += temp;
             }
-          
         }
-
-        return ans;
+        return res;
     }
 };
