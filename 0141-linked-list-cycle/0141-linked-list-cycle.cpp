e7a1f0f2d44ed7eed712584ab300c57159444243
Shivam <officialxshivam45@gmail.com>
2025-10-16 19:19:41 +0530

    Time: 3 ms (98.85%), Space: 12 MB (22.66%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,14 +9,23 @@
 class Solution {
 public:
     bool hasCycle(ListNode* head) {
-        if(head == nullptr || head->next == nullptr) return false;
-        ListNode *slow = head, *fast = head->next;
-        while (fast != nullptr && fast->next != nullptr) {
-            if (slow == fast)
+        // this code just tells the presence of cycle not the entry point of
+        // cycle
+
+        if (head == NULL || head->next == NULL) {
+            return false;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (slow != NULL and fast != NULL and fast->next != NULL) {
+            if (slow == fast) {
                 return true;
+            }
             slow = slow->next;
             fast = fast->next->next;
         }
+
         return false;
     }
 };
