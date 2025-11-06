0bc16c4d1e299a16ad40d24854d1481680cef2a5
Shivam <officialxshivam45@gmail.com>
2025-11-06 18:47:13 +0530

    Time: 0 ms (100%), Space: 9 MB (97.32%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
     int myAtoi(string s) {
         int n = s.length();
-        int i = 0;
-        bool neg = false;
+        int i = 0, sign = 1;
         long long res = 0;
         while (i < n && s[i] == ' ')
             i++;
-        if (i < n && s[i] == '-') {
+        if (i < n && (s[i] == '-' || s[i] == '+')) {
+            sign = (s[i] == '-') ? -1 : 1;
             i++;
-            neg = true;
-        } else if (i < n && s[i] == '+') {
-            i++;
-            neg = false;
         }
         while (i < n && s[i] == '0')
             i++;
         while (i < n && isdigit(s[i])) {
-            if (!neg && res > INT_MAX)
-                return INT_MAX;
-            if (neg && -res < INT_MIN)
-                return INT_MIN;
             res = (res * 10) + (s[i] - '0');
+            if (sign * res >= INT_MAX)
+                return INT_MAX;
+            if (sign * res <= INT_MIN)
+                return INT_MIN;
             i++;
         }
-        if (!neg && res > INT_MAX)
-            return INT_MAX;
-        if (neg && -res < INT_MIN)
-            return INT_MIN;
-        return (neg) ? -res : res;
+        return (int)sign * res;
     }
 };
