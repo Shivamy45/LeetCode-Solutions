6897f2f1a21ff7515c0033b8604d9db3d4359565
Shivam <officialxshivam45@gmail.com>
2025-11-06 23:37:32 +0530

    Time: 7 ms (90.01%), Space: 20.7 MB (84.17%) - LeetHub

--- 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.cpp
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    void helperValidString(int n, string s, vector<string>& res) {
+    void helperValidString(int n, string &s, vector<string>& res) {
         if (s.size() == n) {
             res.push_back(s);
             return;
         }
-        if (s.empty() || s.back() != '0')
-            helperValidString(n, s + '0', res);
-        helperValidString(n, s + '1', res);
+        if (s.empty() || s.back() != '0'){
+            s.push_back('0');
+            helperValidString(n, s, res);
+            s.pop_back();
+        }
+        s.push_back('1');
+        helperValidString(n, s, res);
+        s.pop_back();
     }
     vector<string> validStrings(int n) {
         vector<string> res;
-        helperValidString(n, "", res);
+        string temp;
+        helperValidString(n, temp, res);
         return res;
     }
 };
