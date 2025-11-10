740503a3b28a7015915591201433e1aa37a9d370
Shivam <officialxshivam45@gmail.com>
2025-11-10 22:49:08 +0530

    Time: 794 ms (31.83%), Space: 11.6 MB (44.62%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool helperExist(vector<vector<char>>& board, string word, int i, int j,
+                     int idx, int n, int m) {
+        if (idx == word.length())
+            return true;
+        if (i >= n || i < 0 || j >= m || j < 0)
+            return false;
+        if (word[idx] != board[i][j])
+            return false;
+        char saved = board[i][j];
+        board[i][j] = '#';
+        bool found = helperExist(board, word, i + 1, j, idx + 1, n, m) ||
+                     helperExist(board, word, i, j + 1, idx + 1, n, m) ||
+                     helperExist(board, word, i - 1, j, idx + 1, n, m) ||
+                     helperExist(board, word, i, j - 1, idx + 1, n, m);
+        board[i][j] = saved;
+        return found;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size(), m = board[0].size();
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                if (helperExist(board, word, i, j, 0, n, m))
+                    return true;
+        return false;
+    }
+};
