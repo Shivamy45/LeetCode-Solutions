f183caadf005c6b35f10ffe895112f90c06b42a4
Shivam <officialxshivam45@gmail.com>
2025-11-10 22:55:17 +0530

    Time: 792 ms (32.13%), Space: 11.6 MB (44.62%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    
-bool helperExist(vector<vector<char>> &board, string word, int i, int j, int idx)
-{
-    if (i >= board.size() || i < 0 || j >= board[0].size() || j < 0)
-        return false;
-    if (word[idx] != board[i][j])
-        return false;
-    if (idx == word.size() - 1)
-        return true;
-    char saved = board[i][j];
-    board[i][j] = '#';
-    bool found = helperExist(board, word, i + 1, j, idx + 1) || helperExist(board, word, i, j + 1, idx + 1) || helperExist(board, word, i - 1, j, idx + 1) || helperExist(board, word, i, j - 1, idx + 1);
-    board[i][j] = saved;
-    return found;
-}
+    bool helperExist(vector<vector<char>>& board, string word, int i, int j,
+                     int idx) {
+        if (i >= board.size() || i < 0 || j >= board[0].size() || j < 0)
+            return false;
+        if (word[idx] != board[i][j])
+            return false;
+        if (idx == word.size() - 1)
+            return true;
+        char saved = board[i][j];
+        board[i][j] = '#';
+        bool found = helperExist(board, word, i + 1, j, idx + 1) ||
+                     helperExist(board, word, i, j + 1, idx + 1) ||
+                     helperExist(board, word, i - 1, j, idx + 1) ||
+                     helperExist(board, word, i, j - 1, idx + 1);
+        board[i][j] = saved;
+        return found;
+    }
 
-bool exist(vector<vector<char>> &board, string word)
-{
-    int n = board.size(), m = board[0].size();
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            if (helperExist(board, word, i, j, 0))
-                return true;
-    return false;
-}
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size(), m = board[0].size();
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                if (helperExist(board, word, i, j, 0))
+                    return true;
+        return false;
+    }
 };
