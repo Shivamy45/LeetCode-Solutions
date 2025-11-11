7c3264405e9cda7d2ffb10ea065d215ad9b80cf6
Shivam <officialxshivam45@gmail.com>
2025-11-11 20:08:55 +0530

    Time: 34 ms (74.99%), Space: 53.4 MB (83.05%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void helperPartition(string &s, vector<vector<string>>& res,
+    void helperPartition(string& s, vector<vector<string>>& res,
                          vector<string>& temp, int idx,
                          vector<vector<bool>>& isPP) {
         if (idx == s.length()) {
@@ -17,14 +17,12 @@ public:
     }
 
     vector<vector<string>> partition(string s) {
-        int n = s.length();
         vector<vector<string>> res;
         vector<string> temp;
-        vector<vector<bool>> isPP(n, vector<bool>(n, false));
-        for (int i = n - 1; i >= 0; i--)
-            for (int j = i; j < n; j++)
-                isPP[i][j] =
-                    (s[i] == s[j] && (j - i < 2 || isPP[i + 1][j - 1]));
+        vector<vector<bool>> isPP(s.length(), vector<bool>(s.length(), false));
+        for (int i = s.length() - 1; i >= 0; i--)
+            for (int j = i; j < s.length(); j++)
+                isPP[i][j] = (s[i] == s[j] && (j - i < 2 || isPP[i + 1][j - 1]));
         helperPartition(s, res, temp, 0, isPP);
         return res;
     }
