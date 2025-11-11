546bf63a5a76c76470f2c3907f8034fa802c1029
Shivam <officialxshivam45@gmail.com>
2025-11-11 20:06:43 +0530

    Time: 51 ms (50.4%), Space: 57.5 MB (57.43%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    bool checkPalindrome(string x) {
-        int n = x.size();
-        for (int i = 0; i < n / 2; i++)
-            if (x[i] != x[n - i - 1])
-                return false;
-        return true;
-    }
-
     void helperPartition(string s, vector<vector<string>>& res,
-                         vector<string>& temp, int idx) {
+                         vector<string>& temp, int idx,
+                         vector<vector<bool>>& isPP) {
         if (idx == s.length()) {
             res.push_back(temp);
             return;
         }
-        string cmp = "";
         for (int i = idx; i < s.length(); i++) {
-            cmp += s[i];
-            if (checkPalindrome(cmp)) {
-                temp.push_back(cmp);
-                helperPartition(s, res, temp, i + 1);
+            if (isPP[idx][i]) {
+                temp.push_back(s.substr(idx, i - idx + 1));
+                helperPartition(s, res, temp, i + 1, isPP);
                 temp.pop_back();
             }
         }
     }
 
     vector<vector<string>> partition(string s) {
+        int n = s.length();
         vector<vector<string>> res;
         vector<string> temp;
-        helperPartition(s, res, temp, 0);
+        vector<vector<bool>> isPP(n, vector<bool>(n, false));
+        for (int i = n - 1; i >= 0; i--)
+            for (int j = i; j < n; j++)
+                isPP[i][j] =
+                    (s[i] == s[j] && (j - i < 2 || isPP[i + 1][j - 1]));
+        helperPartition(s, res, temp, 0, isPP);
         return res;
     }
 };
