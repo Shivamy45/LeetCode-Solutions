bf13eaf6991d7088be65e7e21dfd6f533a979684
Shivam <officialxshivam45@gmail.com>
2025-11-12 20:03:26 +0530

    Time: 0 ms (100%), Space: 10.3 MB (53.6%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,50 +1,34 @@
 class Solution {
 public:
-    bool isSafe(vector<string>& board, int row, int col) {
-        int rowCopy = row, colCopy = col;
-        // left
-        while (colCopy >= 0) {
-            if (board[row][colCopy] == 'Q')
-                return false;
-            colCopy--;
-        }
-        // upper diag
-        colCopy = col;
-        while (rowCopy >= 0 && colCopy >= 0) {
-            if (board[rowCopy][colCopy] == 'Q')
-                return false;
-            rowCopy--;
-            colCopy--;
-        }
-        rowCopy = row;
-        colCopy = col;
-        while (rowCopy < board.size() && colCopy >= 0) {
-            if (board[rowCopy][colCopy] == 'Q')
-                return false;
-            rowCopy++;
-            colCopy--;
-        }
-        return true;
-    }
-
     void helperSolveNQueens(vector<vector<string>>& res, vector<string>& board,
-                            int col, int n) {
+                            int col, int n, vector<bool>& left,
+                            vector<bool>& lowerDiag, vector<bool>& upperDiag) {
         if (col == n) {
             res.push_back(board);
             return;
         }
         for (int row = 0; row < n; row++) {
-            if (isSafe(board, row, col)) {
+            if (!(upperDiag[n - 1 + col - row] || lowerDiag[row + col] ||
+                  left[row])) {
+                left[row] = lowerDiag[row + col] =
+                    upperDiag[n - 1 + col - row] = true;
                 board[row][col] = 'Q';
-                helperSolveNQueens(res, board, col + 1, n);
+                helperSolveNQueens(res, board, col + 1, n, left, lowerDiag,
+                                   upperDiag);
                 board[row][col] = '.';
+                upperDiag[n - 1 + col - row] = lowerDiag[row + col] =
+                    left[row] = false;
             }
         }
     }
+
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> res;
         vector<string> board(n, string(n, '.'));
-        helperSolveNQueens(res, board, 0, n);
+        vector<bool> left(n, false);
+        vector<bool> lowerDiag(n * 2 + 1, false);
+        vector<bool> upperDiag(n * 2 + 1, false);
+        helperSolveNQueens(res, board, 0, n, left, lowerDiag, upperDiag);
         return res;
     }
 };
