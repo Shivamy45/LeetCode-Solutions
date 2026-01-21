31650640b7e51beabde3b4d1ece7309a3962dfe8
Shivam <officialxshivam45@gmail.com>
2026-01-21 10:48:37 +0530

    Time: 0 ms (100%), Space: 28.1 MB (83.5%) - LeetHub

--- 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    int helper(int n) {
-        if (n == 2)
-            return -1;
-        int pos = 0;
-        while (n > 0 && ((n >> pos) & 1))
-            pos++;
-        cout << pos << endl;
-        return n ^ (1 << (pos - 1));
-    }
     vector<int> minBitwiseArray(vector<int>& nums) {
         int n = nums.size();
         vector<int> res(n);
         for (int i = 0; i < n; i++) {
-            res[i] = helper(nums[i]);
+            if (nums[i] == 2)
+                res[i] = -1;
+            else
+                res[i] = (~(((nums[i] + 1) & (~nums[i])) >> 1) & nums[i]);
         }
         return res;
     }
