63785930325356086600be8f283cccc2aae855ec
Shivam <officialxshivam45@gmail.com>
2026-02-05 00:41:11 +0530

    Time: 76 ms (59.41%), Space: 172.8 MB (93.8%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,70 @@
+struct Node {
+    int key;
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int k, int v) {
+        key = k;
+        val = v;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+class LRUCache {
+public:
+    int size, curr = 0;
+    unordered_map<int, Node*> mpp;
+    Node* head = new Node(-1, -1);
+    Node* tail = new Node(-1, -1);
+    LRUCache(int capacity) {
+        head->next = tail;
+        tail->prev = head;
+        size = capacity;
+    }
+
+    int get(int key) {
+        if (mpp.find(key) != mpp.end()) {
+            mpp[key]->prev->next = mpp[key]->next;
+            mpp[key]->next->prev = mpp[key]->prev;
+            mpp[key]->next = tail;
+            mpp[key]->prev = tail->prev;
+            tail->prev->next = mpp[key];
+            tail->prev = mpp[key];
+            return mpp[key]->val;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if (mpp.find(key) != mpp.end()) {
+            mpp[key]->prev->next = mpp[key]->next;
+            mpp[key]->next->prev = mpp[key]->prev;
+            mpp[key]->next = tail;
+            mpp[key]->prev = tail->prev;
+            tail->prev->next = mpp[key];
+            tail->prev = mpp[key];
+            mpp[key]->val = value;
+            return;
+        }
+        if (curr == size) {
+            mpp.erase(head->next->key);
+            head->next->next->prev = head;
+            head->next = head->next->next;
+            curr--;
+        }
+        Node* newNode = new Node(key, value);
+        tail->prev->next = newNode;
+        newNode->prev = tail->prev;
+        tail->prev = newNode;
+        newNode->next = tail;
+        mpp[key] = newNode;
+        curr++;
+    }
+};
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
