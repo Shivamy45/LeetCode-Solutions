625699a53099a7233ad22c90a7016cabfd9485e4
Shivam <officialxshivam45@gmail.com>
2026-02-07 11:06:56 +0530

    Time: 26 ms (58.28%), Space: 55.2 MB (10.5%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -6,11 +6,9 @@ public:
         vector<int> bS(n, 0);
         for(int i = 1; i < n; i++){
             bS[i] = bS[i - 1] + ((s[i - 1] == 'b') ? 1 : 0);
+            aS[n - i - 1] = aS[n - i] + ((s[n - i] == 'a') ? 1 : 0);
         }
-        for(int i = n - 2; i >= 0; i--){
-            aS[i] = aS[i + 1] + ((s[i + 1] == 'a') ? 1 : 0);
-        }
-        int res = 1e9;
+        int res = aS[0] + bS[0];
         for(int i = 0; i < n; i++){
             res = min(res, aS[i] + bS[i]);
         }
