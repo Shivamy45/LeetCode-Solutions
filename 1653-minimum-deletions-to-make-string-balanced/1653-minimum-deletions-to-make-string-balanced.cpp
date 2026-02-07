e0716e83599ed5da74e6f741cb99957794770198
Shivam <officialxshivam45@gmail.com>
2026-02-07 12:06:58 +0530

    Time: 24 ms (60.5%), Space: 40.5 MB (27.66%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -9,7 +9,7 @@ public:
         }
         int res = aS[0];
         for(int i = 0; i < n; i++){
-            if(i > 0 && s[i-1] == 'b') bS++; 
+            if(i > 0 && s[i - 1] == 'b') bS++; 
             res = min(res, aS[i] + bS);
         }
         return res;
