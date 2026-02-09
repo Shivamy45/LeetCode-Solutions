62b48b3fa6a34e94aeaf3af4c426c64961162459
Shivam <officialxshivam45@gmail.com>
2026-02-09 22:27:57 +0530

    Time: 4 ms (84.93%), Space: 48.6 MB (83.04%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -11,22 +11,22 @@
  */
 class Solution {
 public:
-    void convertTreeToArr(TreeNode *root, vector<int>& arr){
+    void convertTreeToArr(TreeNode *root, vector<TreeNode*>& arr){
         if(!root) return;
         convertTreeToArr(root->left, arr);
-        arr.push_back(root->val);
+        arr.emplace_back(root);
         convertTreeToArr(root->right, arr);
     }
-    TreeNode* convertToBST(vector<int>&arr, int low, int high){
+    TreeNode* convertToBST(vector<TreeNode*>&arr, int low, int high){
         if(low > high) return nullptr;
         int mid = (low + high) / 2;
-        TreeNode *node = new TreeNode(arr[mid]);
+        TreeNode *node = arr[mid];
         node->left = convertToBST(arr, low, mid - 1);
         node->right = convertToBST(arr, mid + 1, high);
         return node;
     }
     TreeNode* balanceBST(TreeNode* root) {
-        vector<int> arr;
+        vector<TreeNode *> arr;
         convertTreeToArr(root, arr);
         TreeNode *bst = convertToBST(arr, 0, arr.size() - 1);
         return bst;
