fa86667426480d1eaf6e5106e41860c4ccf49610
Shivam <officialxshivam45@gmail.com>
2026-02-10 14:30:47 +0530

    Time: 10 ms (47.49%), Space: 11.1 MB (56.59%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s) {
-        int n = s.length();
-        int i = 0, cnt = 0, a = 0, b = 0, c = 0;
-        for (int j = 0; j < n; j++) {
-            if (s[j] == 'a')
-                a++;
-            else if (s[j] == 'b')
-                b++;
-            else if (s[j] == 'c')
-                c++;
-
-            while (a > 0 && b > 0 && c > 0) {
-                if (s[i] == 'a')
-                    a--;
-                else if (s[i] == 'b')
-                    b--;
-                else if (s[i] == 'c')
-                    c--;
-                i++;
-            }
-            cnt += (j - i + 1);
+    int numberOfSubstrings(string s)
+{
+    int n = s.length();
+    int i = 0, cnt = 0;
+    vector<int> freq(3, 0);
+    for (int j = 0; j < n; j++)
+    {
+        freq[s[j] - 'a']++;
+        while (freq[0] && freq[1] && freq[2])
+        {
+            cnt += (n - j);
+            freq[s[i] - 'a']--;
+            i++;
         }
-        long long total = (long long)n * (n + 1) / 2;
-        return total - cnt;
     }
+    return cnt;
+}
 };
