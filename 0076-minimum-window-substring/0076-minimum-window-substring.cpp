e94526647a7c31f77cdb795416858b035c6f80db
Shivam <officialxshivam45@gmail.com>
2026-02-11 11:58:06 +0530

    Time: 3 ms (79.72%), Space: 11.3 MB (93.79%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        int n = s.length(), m = t.length();
-        unordered_map<char, int> freq;
-        for (char c : t)
-            freq[c]++;
-        int i = 0, matched = 0;
-        int start = -1, len = -1;
-        for (int j = 0; j < n; j++) {
-            if (freq.find(s[j]) != freq.end()) {
-                freq[s[j]]--;
-                if (freq[s[j]] >= 0)
-                    matched++;
+    string minWindow(string s, string t)
+{
+    int n = s.length(), m = t.length();
+    if(n < m)
+        return "";
+    vector<int> freq(128, 0);
+    for (char c : t)
+        freq[c]++;
+    int i = 0, matched = 0;
+    int start = 0, len = INT_MAX;
+    for (int j = 0; j < n; j++)
+    {
+        if (freq[s[j]] > 0)
+            matched++;
+        freq[s[j]]--;
+        while (matched == m)
+        {
+            if (j - i + 1 < len)
+            {
+                start = i;
+                len = j - i + 1;
             }
-            while (matched == m) {
-                if (start == -1 || len > j - i + 1) {
-                    start = i;
-                    len = j - i + 1;
-                }
-                if (freq.find(s[i]) != freq.end()) {
-                    freq[s[i]]++;
-                    if (freq[s[i]] > 0)
-                        matched--;
-                }
-                i++;
-            }
+            freq[s[i]]++;
+            if (freq[s[i]] > 0)
+                matched--;
+            i++;
         }
-        return (start == -1) ? "" : s.substr(start, len);
     }
+    return (len == INT_MAX) ? "" : s.substr(start, len);
+}
 };
