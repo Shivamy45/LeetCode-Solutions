cb909daeffd99968e1e5fb1b92c2b44c481fb885
Shivam <officialxshivam45@gmail.com>
2026-02-15 23:18:23 +0530

    Time: 3 ms (22.17%), Space: 8.7 MB (95.88%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         int n = a.length(), m = b.length();
-        int i = n - 1, j = m - 1, c= 0;
-        string res  = "";
-        while(i >= 0 || j >= 0 || c != 0){
-            int an = (i >= 0) ? a[i] - '0' : 0;
-            int bn = (j >= 0) ? b[j] - '0' : 0;
-            res = to_string((an + bn + c) % 2) + res;
-            if(an + bn + c >= 2){
-                c = 1;
-            } else {
-                c = 0;
-            }
-            i--; j--;
+        int i = n - 1, j = m - 1, c = 0;
+        string res = "";
+        while (i >= 0 || j >= 0 || c != 0) {
+            if (i >= 0)
+                c += a[i] - '0';
+            if (j >= 0)
+                c += b[j] - '0';
+            res += (c % 2) + '0';
+            c /= 2;
+            i--;
+            j--;
         }
+        reverse(res.begin(), res.end());
         return res;
     }
 };
