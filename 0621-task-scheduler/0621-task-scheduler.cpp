4c6c19519eba3c187f379064d018ce5ebd3176d7
Shivam <officialxshivam45@gmail.com>
2026-02-19 11:13:56 +0530

    Time: 0 ms (100%), Space: 38.2 MB (82.57%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         vector<int> freq(26, 0);
-        for (char c : tasks)
-            freq[c - 'A']++;
-
-        int fmax = 0, k = 0;
+        for (char task : tasks)
+            freq[task - 'A']++;
+        int maxf = 0, k = 0;
         for (int f : freq) {
-            if (f > fmax) {
-                fmax = f;
+            if (f > maxf) {
+                maxf = f;
                 k = 1;
-            } else if (f == fmax && f > 0) {
+            } else if (f == maxf && f > 0)
                 k++;
-            }
         }
-
-        return max((int)tasks.size(), (fmax - 1) * (n + 1) + k);
+        return max((int)tasks.size(), (maxf - 1) * (n + 1) + k);
     }
 };
