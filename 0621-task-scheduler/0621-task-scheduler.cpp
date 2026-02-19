b97b8865b786c5413043ee9cc1daaaf88510e714
Shivam <officialxshivam45@gmail.com>
2026-02-19 11:01:27 +0530

    Time: 39 ms (31.23%), Space: 47.7 MB (30.18%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
-    int leastInterval(vector<char> &tasks, int n)
-{
-    unordered_map<char, int> mpp;
-    for (char c : tasks)
-        mpp[c]++;
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char, int> mpp;
+        for (char c : tasks)
+            mpp[c]++;
 
-    priority_queue<int> pq;
-    for (auto &it : mpp)
-        pq.push(it.second);
+        priority_queue<int> pq;
+        for (auto& it : mpp)
+            pq.push(it.second);
 
-    int time = 0;
+        int time = 0;
 
-    while (!pq.empty())
-    {
-        vector<int> temp;
-        int slots = n + 1;
+        while (!pq.empty()) {
+            vector<int> temp;
+            int slots = n + 1;
 
-        while (slots-- && !pq.empty())
-        {
-            int x = pq.top();
-            pq.pop();
-            if (--x > 0)
-                temp.push_back(x);
-            time++;
+            while (slots-- && !pq.empty()) {
+                int x = pq.top();
+                pq.pop();
+                if (--x > 0)
+                    temp.push_back(x);
+                time++;
+            }
+
+            for (int x : temp)
+                pq.push(x);
+
+            if (pq.empty())
+                break;
+
+            time += slots + 1;
         }
 
-        for (int x : temp)
-            pq.push(x);
-
-        if (pq.empty())
-            break;
-
-        time += slots + 1;
+        return time;
     }
-
-    return time;
-}
 };
