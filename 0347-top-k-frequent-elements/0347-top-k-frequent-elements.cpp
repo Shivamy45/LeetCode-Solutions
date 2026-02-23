d1021af7dd75af8b0d63feb9af05a4100a7c8997
Shivam <officialxshivam45@gmail.com>
2026-02-23 11:21:05 +0530

    Time: 4 ms (35.24%), Space: 17.8 MB (97.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,13 +4,18 @@ public:
         unordered_map<int, int> mpp;
         for (int& num : nums)
             mpp[num]++;
-        priority_queue<pair<int, int>> pq;
-        for (auto& it : mpp)
-            pq.push(make_pair(it.second, it.first));
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            minHeap;
+        for (auto& it : mpp) {
+            minHeap.push(make_pair(it.second, it.first));
+            if (minHeap.size() > k)
+                minHeap.pop();
+        }
         vector<int> res;
-        while (k--) {
-            res.push_back(pq.top().second);
-            pq.pop();
+        while (k-- && !minHeap.empty()) {
+            res.push_back(minHeap.top().second);
+            minHeap.pop();
         }
         return res;
     }
