2f5f113dc60d8c94122c74ec8db2a03964e20b50
Shivam <officialxshivam45@gmail.com>
2026-02-24 00:16:51 +0530

    Time: 383 ms (66.3%), Space: 119 MB (59.96%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -3,10 +3,10 @@ public:
     bool hasAllCodes(string s, int k) {
         if (k >= s.length())
             return false;
-        set<string> binary;
+        unordered_set<string> binary;
         for (int i = 0; i <= s.length() - k; i++) {
             binary.insert(s.substr(i, k));
         }
-        return binary.size() == pow(2, k);
+        return binary.size() == (1 << k);
     }
 };
