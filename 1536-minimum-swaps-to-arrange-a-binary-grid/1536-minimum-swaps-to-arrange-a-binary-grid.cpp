20996e771a4574795425b43f8554466e1d589cba
Shivam <officialxshivam45@gmail.com>
2026-03-02 21:07:09 +0530

    Time: 1 ms (36.59%), Space: 30.1 MB (24.39%) - LeetHub

--- 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -4,24 +4,18 @@ public:
         int n = grid.size();
         vector<int>mostRight(n);
         for(int i = 0; i< n; i++){
-            for(int j = n - 1; j >= 0; j--){
-                if(grid[i][j] == 1) {
-                    mostRight[i] = j;
-                    break;
-                }
+            int j = n - 1;
+            while(j >= 0 && grid[i][j] == 0){
+                j--;
             }
+            mostRight[i] = j;
         }
         int swaps = 0;
         for(int i = 0; i < n; i++){
             if(mostRight[i] <= i) continue;
-            int j = -1;
-            for(int k = i + 1; k < n; k++){
-                if(mostRight[k] <= i) {
-                    j = k;
-                    break;
-                }
-            }
-            if(j == -1) return -1;
+            int j = i;
+            while(j < n && mostRight[j] > i) j++;
+            if(j == n) return -1;
             while(j > i){
                 swap(mostRight[j], mostRight[j - 1]);
                 swaps++;
