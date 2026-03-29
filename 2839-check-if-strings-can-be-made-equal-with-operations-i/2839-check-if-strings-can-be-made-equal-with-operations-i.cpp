591ec5c5a93e390406b0cb298623017d40d168f2
Shivam <officialxshivam45@gmail.com>
2026-03-29 23:26:03 +0530

    Time: 0 ms (100%), Space: 8.3 MB (67.1%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-    bool helper(string &s1, string &s2, int i){
-        if(s1 == s2) return true;
-        if(i == 2) return false;
-        bool flag = false;
-        flag = flag || helper(s1, s2, i + 1);
-        swap(s1[i], s1[i + 2]);
-        flag = flag || helper(s1, s2, i + 1);
-        return flag;
-    }
     bool canBeEqual(string s1, string s2) {
-        return helper(s1, s2, 0);
+        for(int i = 0; i < 2; i++){
+            if(s1[i] != s2[i]){
+                swap(s1[i], s1[i + 2]);
+            }
+        }
+        return s1 == s2;
     }
 };
