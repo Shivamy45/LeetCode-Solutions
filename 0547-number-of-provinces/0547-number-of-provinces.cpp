efd588d40cf5f1f2a1a2cbbc7e3565ddada75ab2
Shivam <officialxshivam45@gmail.com>
2026-04-23 00:15:21 +0530

    Time: 15 ms (5.84%), Space: 33 MB (5.03%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     void bfs(vector<vector<int>> mat, vector<bool>& vis, int idx) {
-        for (int i = 0; i < mat[idx].size(); i++) {
-            if (mat[idx][i] == 1 && !vis[i]) {
-                vis[i] = true;
-                bfs(mat, vis, i);
+        queue<int> q;
+        q.push(idx);
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            for (int i = 0; i < mat.size(); i++) {
+                if (mat[node][i] == 1 && !vis[i]) {
+                    q.push(i);
+                    vis[i] = true;
+                }
             }
         }
     }
