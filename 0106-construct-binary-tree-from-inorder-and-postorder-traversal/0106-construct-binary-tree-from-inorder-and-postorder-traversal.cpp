8a2091096d9348353112d93fa7a315dd8affc475
Shivam <officialxshivam45@gmail.com>
2026-05-20 16:45:38 +0530

    Time: 0 ms (100%), Space: 27.5 MB (55.33%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -13,18 +13,12 @@
 class Solution {
 public:
     TreeNode *build(vector<int>&inorder, vector<int>&postorder, int &idx, int l, int r, unordered_map<int, int>&mpp){
-        if(l > r || idx == -1) return nullptr;
-        int val = postorder[idx];
+        if(l > r) return nullptr;
+        int val = postorder[idx--];
         TreeNode* root = new TreeNode(val);
         int i = mpp[val];
-        if(i + 1 < r){
-            --idx;
-            root->right = build(inorder, postorder, idx, i, r, mpp);
-        }
-        if(i - 1 > l){
-            --idx;
-            root->left = build(inorder, postorder, idx, l, i, mpp);
-        }
+        root->right = build(inorder, postorder, idx, i + 1, r, mpp);
+        root->left = build(inorder, postorder, idx, l, i - 1, mpp);
         return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
@@ -34,6 +28,6 @@ public:
             mpp[inorder[i]] = i;
         }
         int idx = n - 1;
-        return build(inorder, postorder, idx, -1, n, mpp);
+        return build(inorder, postorder, idx, 0, n - 1, mpp);
     }
 };
