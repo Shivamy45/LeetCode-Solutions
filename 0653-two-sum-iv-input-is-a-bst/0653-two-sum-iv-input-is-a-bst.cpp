76fc6e3ed15a7bff69f1e7e6d60f41ee76b13bd0
Shivam <officialxshivam45@gmail.com>
2026-06-24 21:57:47 +0530

    Time: 7 ms (29.48%), Space: 38.9 MB (28.98%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -12,12 +12,12 @@
  */
 class Solution {
 public:
-    unordered_map<int, TreeNode*> mpp;
+    unordered_map<int, int> mpp;
     bool findTarget(TreeNode* root, int k) {
         if(!root) return false;
         if (mpp.count(k - root->val) == 1)
             return true;
-        mpp[root->val] = root;
+        mpp[root->val] = 1;
         return findTarget(root->left, k) || findTarget(root->right, k);
     }
 };
