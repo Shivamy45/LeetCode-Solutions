8ff29263a7515f0deb27309bc16c9672a9e75b89
Shivam <officialxshivam45@gmail.com>
2026-07-16 17:02:57 +0530

    Time: 1 ms (77.46%), Space: 11.5 MB (76.85%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool helperWordBreak(string& s, unordered_set<string>& words, int idx,
-                         vector<int>& dp) {
-        if (idx == s.length())
-            return true;
-        if (dp[idx] != -1)
-            return dp[idx];
-        for (int i = idx; i < s.length(); i++) {
-            string word = s.substr(idx, i - idx + 1);
-            if (words.count(word) && helperWordBreak(s, words, i + 1, dp)) {
-                return dp[idx] = true;
-            }
+                     vector<int>& dp, int maxLen) {
+    if (idx == s.length()) return true;
+    if (dp[idx] != -1) return dp[idx];
+    for (int i = idx; i < s.length(); i++) {
+        if (i - idx + 1 > maxLen) break;
+        string word = s.substr(idx, i - idx + 1);
+        if (words.count(word) && helperWordBreak(s, words, i + 1, dp, maxLen)) {
+            return dp[idx] = true;
         }
-        return dp[idx] = false;
     }
+    return dp[idx] = false;
+}
 
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> words{begin(wordDict), end(wordDict)};
-        vector<int> dp(s.length(), -1);
-        return helperWordBreak(s, words, 0, dp);
-    }
+bool wordBreak(string s, vector<string>& wordDict) {
+    unordered_set<string> words{begin(wordDict), end(wordDict)};
+    int maxLen = 0;
+    for (string a : words) maxLen = max(maxLen, (int)a.length());
+    vector<int> dp(s.length(), -1);
+    return helperWordBreak(s, words, 0, dp, maxLen);
+}
 };
